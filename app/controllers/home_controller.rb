9240e74daf88e945f50dd590ec793f6f547ff342
Ethan Edmond <eedmond@uccs.edu>
2021-09-27 13:04:23 -0600

    Got pdf updating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,13 @@ class HomeController < ApplicationController
     
   end
   def pdf
-    # resume_file = File.open('resume_template_mine.tex.erb')
-    # resume = ERB.new(resume_file.read).result
-    # resume_file.close
+    resume_file = File.open('resume_template_mine.tex.erb')
+    resume = ERB.new(resume_file.read).result
+    resume_file.close
+    File.write('tmp/resume.tex', resume)
+    system 'pdflatex -output-directory tmp tmp/resume.tex'
+    # system 'ls'
     # print resume
-    send_file('app/views/home/resume.pdf', :filename => 'resume.pdf', :disposition => 'inline', :type => 'application/pdf')
+    send_file('tmp/resume.pdf', :filename => 'resume.pdf', :disposition => 'inline', :type => 'application/pdf')
   end
 end
