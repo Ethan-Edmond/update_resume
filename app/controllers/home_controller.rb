d6c7048e7859e1d4cece5efe4cf0399d6b3f9408
Ethan Edmond <eedmond@uccs.edu>
2021-09-27 14:11:03 -0600

    got title on resume to change based on url query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,16 @@
+ResumeObj = Hash.new
 class HomeController < ApplicationController
   def index
     
   end
   def pdf
+    ResumeObj[:title] = params[:title]
     resume_file = File.open('resume_template_mine.tex.erb')
     resume = ERB.new(resume_file.read).result
     resume_file.close
     File.write('tmp/resume.tex', resume)
     system 'pdflatex -output-directory tmp tmp/resume.tex'
+    
     # system 'ls'
     # print resume
     send_file('tmp/resume.pdf', :filename => 'resume.pdf', :disposition => 'inline', :type => 'application/pdf')
