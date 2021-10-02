351f71c0802b61f0949facf4ea39ff9c1e1866e9
Ethan Edmond <eedmond@uccs.edu>
2021-10-01 23:25:31 -0600

    Made username unique

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,10 @@ ActiveRecord::Schema.define(version: 2021_10_01_005152) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_hash"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
