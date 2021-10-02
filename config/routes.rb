c4afe2ce8ec3e2aca769500b598baade79e0ead3
Ethan Edmond <eedmond@uccs.edu>
2021-10-01 22:07:55 -0600

    Added login and register routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'pdf', to: 'home#pdf'
+  get 'login', to: 'security#login'
+  get 'register', to: 'security#register'
 end
